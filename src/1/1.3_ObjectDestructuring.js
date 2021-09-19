aeb19bcf0b1b4e8f3a1b96e1927160d90f7c9a28
seongguk <live8681@naver.com>
2021-09-19 21:58:35 +0900

    #1.3 Object Destructuring

---
+++ src/1/1.3_ObjectDestructuring.js
@@ -0,0 +1,16 @@
+// #1.3 Object Destructuring
+const human = {
+    name: "Nit",
+    lastName: "Pick",
+    nationality: "Wish i was korean",
+    favFood: {
+        breakfast: "Sang",
+        lunch: "Doncas",
+        dinner: "Sang + Doncas"
+    }
+}
+
+//const nmae = human.name; before
+
+const { name, lastName, nationality: difName, favFood: { dinner, breakfast, lunch } } = human;
+console.log(name, lastName, difName, dinner, breakfast, lunch);
