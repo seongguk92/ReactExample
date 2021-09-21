30e07f311eb03c1762c88576b13a3c0fa61bc60b
seongguk <live8681@naver.com>
2021-09-21 23:01:29 +0900

    #3.1 CSS in React part Two

--- nomflix/src/Components/App.js
+++ nomflix/src/Components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
-import Header from "Components/Header ";
+import Header from "Components/Header";
 
 class App extends Component {
   render() {
--- nomflix/src/Components/Header/Header.js
+++ nomflix/src/Components/Header/Header.js
@@ -1,10 +1,9 @@
 import React from "react";
-import "styles.css";
-
+import styles from "./Header.module.css";
 
 export default () => (
-    <header className="nav">
-        <ul>
+    <header>
+        <ul className={styles.navList}>
             <li>
                 <a href="/">
                     Movies
