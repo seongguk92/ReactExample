3f185965de518274b142f4916f7285cc51481efb
seongguk <live8681@naver.com>
2021-09-24 00:34:16 +0900

    Detail Container part One

--- nomflix/src/Routes/Detail/DetailContainer.js
+++ nomflix/src/Routes/Detail/DetailContainer.js
@@ -8,6 +8,20 @@ export default class extends React.Component {
         loading: true
     };
 
+    async componentDidMount() {
+        const { match:
+            {
+                params: { id }
+            },
+            history: { push }
+        } = this.props;
+
+        const parsedId = parseInt(id);
+        if (isNaN(parsedId)) {
+            push("/");
+        }
+    };
+
     render() {
         const { result, error, loading } = this.state;
         return (
