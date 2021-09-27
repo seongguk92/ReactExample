55ce821eb57d7c1130f8370485dc8b5d13afe927
seongguk <live8681@naver.com>
2021-09-27 23:31:07 +0900

    #6.7 Poster Component part Two

--- nomflix/src/Components/Poster.js
+++ nomflix/src/Components/Poster.js
@@ -3,27 +3,59 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-const Container = styled.div``;
-
-const ImageContainer = styled.div``;
-
-const Image = styled.div``;
-
-const Rating = styled.span``;
-
-const Title = styled.span``;
-
-const Year = styled.span``;
+const Container = styled.div`
+    font-size: 12px;
+`;
 
 
 
+const Image = styled.div`
+    background-image: url(${props => props.bgUrl});
+    height: 180px;
+    background-size: cover;
+    border-radius: 4px;
+    background-position: center center;
+    transition: opacity 0.1s linear;
+`;
 
+const Rating = styled.span`
+    bottom:5px;
+    right:5px;
+    position: absolute;
+    opacity: 0;
+    transition: opacity 0.1s linear;
+`;
+
+const ImageContainer = styled.div`
+    margin-bottom: 5px;
+    position: relative;
+    &:hover{
+        ${Image}{
+            opacity: 0.3;
+        }
+        ${Rating}{
+            opacity: 1;
+        }
+    }
+`;
+
+const Title = styled.span`
+    display: block;
+    font-size: 12px;
+    margin-bottom: 3px;
+`;
+
+const Year = styled.span`
+    font-size: 10px;
+    color:rgba(255,255,255,0.5);
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
     <Link to={isMovie ? `/Movie/${id}` : `/show/${id}`}>
         <Container>
             <ImageContainer>
-                <Image bgUrl={imageUrl} />
+                <Image bgUrl={imageUrl ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+                    : require("../assets/noPosterSmall.png").default} />
                 <Rating>
                     <span role="img" aria-label="rating">
                         ⭐
@@ -31,7 +63,11 @@ const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
                     {rating}/10
                 </Rating>
             </ImageContainer>
-            <Title>{title}</Title>
+            <Title>
+                {title.length > 18 ?
+                    `${title.substring(0, 18)}...`
+                    : title}
+            </Title>
             <Year>{year}</Year>
         </Container>
     </Link>
