bb59acc5249578979747a01645e8adcb1b30ae4d
seongguk <live8681@naver.com>
2021-09-30 23:31:42 +0900

    #6.10 React Helmet

--- nomflix/src/Routes/Home/HomePresenter.js
+++ nomflix/src/Routes/Home/HomePresenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types"
 import styled from "styled-components";
+import Helmet from "react-helmet";
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
@@ -12,62 +13,73 @@ const Container = styled.div`
 `;
 
 
-const HomePresenter = ({ nowPlaying, popular, upcoming, error, loading }) =>
-    loading ? (
-        <Loader />
-    ) : (
-        <Container>
-            {nowPlaying && nowPlaying.length > 0 && (
-                <Section title="Now Playing">
-                    {nowPlaying.map
-                        (movie => (
-                            <Poster
-                                key={movie.id}
-                                id={movie.id}
-                                imageUrl={movie.poster_path}
-                                title={movie.original_title}
-                                rating={movie.vote_average}
-                                year={movie.release_date && movie.release_date.substring(0, 4)}
-                                isMovie={true}
-                            />
-                        ))}
-                </Section>
-            )}
-            {upcoming && upcoming.length > 0 && (
-                <Section title="Upcoming movies">
-                    {upcoming.map
-                        (movie => (
-                            <Poster
-                                key={movie.id}
-                                id={movie.id}
-                                imageUrl={movie.poster_path}
-                                title={movie.original_title}
-                                rating={movie.vote_average}
-                                year={movie.release_date && movie.release_date.substring(0, 4)}
-                                isMovie={true}
-                            />
-                        ))}
-                </Section>
-            )}
-            {popular && popular.length > 0 && (
-                <Section title="Popular movies">
-                    {popular.map
-                        (movie => (
-                            <Poster
-                                key={movie.id}
-                                id={movie.id}
-                                imageUrl={movie.poster_path}
-                                title={movie.original_title}
-                                rating={movie.vote_average}
-                                year={movie.release_date && movie.release_date.substring(0, 4)}
-                                isMovie={true}
-                            />
-                        ))}
-                </Section>
-            )}
-            {error && <Message color="#e74c3c" text={error} />}
-        </Container>
-    );
+const HomePresenter = ({ nowPlaying, popular, upcoming, error, loading }) => (
+    <>
+        <Helmet>
+            <title>Movies | Nomflix</title>
+        </Helmet>
+
+        {loading ? (
+            <Loader />
+        ) : (
+            <Container>
+                <Helmet>
+                    <title>Movies | Nomflix</title>
+                </Helmet>
+                {nowPlaying && nowPlaying.length > 0 && (
+                    <Section title="Now Playing">
+                        {nowPlaying.map
+                            (movie => (
+                                <Poster
+                                    key={movie.id}
+                                    id={movie.id}
+                                    imageUrl={movie.poster_path}
+                                    title={movie.original_title}
+                                    rating={movie.vote_average}
+                                    year={movie.release_date && movie.release_date.substring(0, 4)}
+                                    isMovie={true}
+                                />
+                            ))}
+                    </Section>
+                )}
+                {upcoming && upcoming.length > 0 && (
+                    <Section title="Upcoming movies">
+                        {upcoming.map
+                            (movie => (
+                                <Poster
+                                    key={movie.id}
+                                    id={movie.id}
+                                    imageUrl={movie.poster_path}
+                                    title={movie.original_title}
+                                    rating={movie.vote_average}
+                                    year={movie.release_date && movie.release_date.substring(0, 4)}
+                                    isMovie={true}
+                                />
+                            ))}
+                    </Section>
+                )}
+                {popular && popular.length > 0 && (
+                    <Section title="Popular movies">
+                        {popular.map
+                            (movie => (
+                                <Poster
+                                    key={movie.id}
+                                    id={movie.id}
+                                    imageUrl={movie.poster_path}
+                                    title={movie.original_title}
+                                    rating={movie.vote_average}
+                                    year={movie.release_date && movie.release_date.substring(0, 4)}
+                                    isMovie={true}
+                                />
+                            ))}
+                    </Section>
+                )}
+                {error && <Message color="#e74c3c" text={error} />}
+            </Container>
+        )}
+    </>
+);
+
 
 HomePresenter.propTypes = {
     nowPlaying: PropTypes.array,
